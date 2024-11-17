fa2b09a07e7abe1a1cffb84bf3a2fbb6590f62d2
Denis Katasonov <diarusfoxs@gmail.com>
2024-11-17 18:31:59 +0200

    Added: initialization of list with products and cashregister

--- SuperMarketDesctopApp/Program.cs
+++ SuperMarketDesctopApp/Program.cs
@@ -1,17 +1,29 @@
+using SupermarketConsoleApp.Classes;
+
 namespace SuperMarketDesctopApp
 {
     internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+    { 
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            List<Product> Products = new List<Product>();
+
+            Product.CreateProduct(Products, "banana", 15);
+            Product.CreateProduct(Products, "Bread", 10);
+            Product.CreateProduct(Products, "Fanta", 36.50);
+            Product.CreateProduct(Products, "Milk", 33.70);
+            Product.CreateProduct(Products, "Chocolate Ice Cream", 30);
+            Product.CreateProduct(Products, "Coca-Cola", 37);
+            Product.CreateProduct(Products, "Pepsi-Cola", 45);
+            Product.CreateProduct(Products, "Cheese", 15);
+            Product.CreateProduct(Products, "Pineapple", 15.50);
+            Product.CreateProduct(Products, "Water", 5.30);
+
+            CashRegister cashRegister = CashRegister.CreateCashRegister(1, "Model_1", Products);
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            Application.Run(new MainForm(Products,cashRegister));
         }
     }
 }
