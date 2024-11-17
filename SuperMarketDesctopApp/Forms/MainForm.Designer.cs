df4d8137bccbef96670877aa975cc06c4435c22a
Denis Katasonov <diarusfoxs@gmail.com>
2024-11-17 18:24:10 +0200

    Updated: added buttons for selecting functions

--- SuperMarketDesctopApp/Forms/MainForm.Designer.cs
+++ SuperMarketDesctopApp/Forms/MainForm.Designer.cs
@@ -33,6 +33,10 @@
             button2 = new Button();
             button3 = new Button();
             button4 = new Button();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
             SuspendLayout();
             // 
             // button1
@@ -43,15 +47,17 @@
             button1.TabIndex = 0;
             button1.Text = "Замовлення";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(287, 56);
+            button2.Location = new Point(351, 56);
             button2.Name = "button2";
             button2.Size = new Size(151, 43);
             button2.TabIndex = 1;
             button2.Text = "Кошти";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
@@ -61,29 +67,75 @@
             button3.TabIndex = 2;
             button3.Text = "Транзакції";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(287, 129);
+            button4.Location = new Point(351, 129);
             button4.Name = "button4";
             button4.Size = new Size(151, 43);
             button4.TabIndex = 3;
             button4.Text = "Додати товар";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(21, 178);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(143, 24);
+            checkBox1.TabIndex = 12;
+            checkBox1.Text = "Оплата карткою";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(170, 178);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(163, 24);
+            checkBox2.TabIndex = 13;
+            checkBox2.Text = "online-замовлення";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(339, 178);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(252, 24);
+            checkBox3.TabIndex = 14;
+            checkBox3.Text = "online-замовлення з доставкою";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(105, 208);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(420, 24);
+            checkBox4.TabIndex = 15;
+            checkBox4.Text = "Розрахунок готівкою при online-замовленні та доставці";
+            checkBox4.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(525, 251);
+            ClientSize = new Size(603, 251);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "MainForm";
             Text = "Каса";
-            Load += MainForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -93,5 +145,9 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
     }
 }
--- SuperMarketDesctopApp/Forms/MainForm.cs
+++ SuperMarketDesctopApp/Forms/MainForm.cs
@@ -1,15 +1,51 @@
+using SupermarketConsoleApp.Classes;
+using SuperMarketDesctopApp.Forms;
+
 namespace SuperMarketDesctopApp
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private List<Product> _products;
+        private CashRegister _cashRegister;
+        public MainForm(List<Product> products, CashRegister cashRegister)
         {
             InitializeComponent();
+            _products = products;
+            _cashRegister = cashRegister;
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private void Check()
         {
-
+            if (checkBox1.Checked == true) { _cashRegister.CardPayment = true; }
+            if (checkBox2.Checked == true) { _cashRegister.OnlineOrder = true; }
+            if (checkBox3.Checked == true) { _cashRegister.OnlineDeliveryOrder = true; }
+            if (checkBox4.Checked == true) { _cashRegister.CashOnlineDeliveryOrder = true; }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AddProductForm addProduct = new AddProductForm(_products);
+            addProduct.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CashForm cash = new CashForm(_cashRegister);
+            cash.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Check();
+            BasketForm basket = new BasketForm(_products, _cashRegister);
+            basket.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TransactionsForm transactions = new TransactionsForm(_cashRegister.GetLogger());
+            transactions.ShowDialog();
+        }
+
     }
 }
