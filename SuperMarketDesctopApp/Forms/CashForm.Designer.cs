882c6e84d3ff8a9527ddb33b703195f1c374aa77
Denis Katasonov <diarusfoxs@gmail.com>
2024-11-17 18:25:06 +0200

    Added: Cash register form and implementation of withdrawal and addition of cash

---
+++ SuperMarketDesctopApp/Forms/CashForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SuperMarketDesctopApp.Forms
+{
+    partial class CashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(105, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Всього на рахунку:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(247, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 1;
+            label2.Text = "amount";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(144, 133);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(125, 27);
+            maskedTextBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(40, 202);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 59);
+            button1.TabIndex = 3;
+            button1.Text = "Поповнити";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(243, 202);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 59);
+            button2.TabIndex = 4;
+            button2.Text = "Зняти";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // CashForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(422, 300);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CashForm";
+            Text = "CashForm";
+            Load += CashForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private MaskedTextBox maskedTextBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ SuperMarketDesctopApp/Forms/CashForm.cs
@@ -0,0 +1,59 @@
+﻿using SupermarketConsoleApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMarketDesctopApp.Forms
+{
+    public partial class CashForm : Form
+    {
+        private CashRegister _cashRegister;
+        private double _amount;
+        public CashForm(CashRegister cashRegister)
+        {
+            InitializeComponent();
+            _cashRegister = cashRegister;
+            _amount = cashRegister.GetAmount();
+
+        }
+
+        private void CashForm_Load(object sender, EventArgs e)
+        {
+            label2.Text = _amount.ToString("F2");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double value;
+
+            if (double.TryParse(maskedTextBox1.Text, out value))
+            {
+                _cashRegister.AddCash(value);
+            }
+            else
+            {
+                MessageBox.Show("Помилка: Невірний формат введення спробуйте ще раз:", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double value;
+
+            if (double.TryParse(maskedTextBox1.Text, out value))
+            {
+                _cashRegister.Withdraw(value);
+            }
+            else
+            {
+                MessageBox.Show("Помилка: Невірний формат введення спробуйте ще раз:", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            };
+        }
+    }
+}
