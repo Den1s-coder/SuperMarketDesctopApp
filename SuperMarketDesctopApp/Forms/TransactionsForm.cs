4a378a418d57965a011b8fa483f543d83328511e
Denis Katasonov <diarusfoxs@gmail.com>
2024-11-17 18:23:08 +0200

    Added: Transaction form and his realisation

---
+++ SuperMarketDesctopApp/Forms/TransactionsForm.cs
@@ -0,0 +1,26 @@
+﻿using SupermarketConsoleApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMarketDesctopApp.Forms
+{
+    public partial class TransactionsForm : Form
+    {
+        private List<Check> _transaction;
+        public TransactionsForm(List<Check> transaction)
+        {
+            InitializeComponent();
+            _transaction = transaction;
+            listBox1_update(_transaction);
+        }
+
+        private void listBox1_update(List<Check> transaction) { }
+    }
+}
