747924efebe798e5cc406e0ffd87cde68a1785d9
Denis Katasonov <diarusfoxs@gmail.com>
2024-11-27 21:09:12 +0200

    Updated:The counting method now takes online status

--- SuperMarketDesctopApp/Payments/Interface/IPaymentForm.cs
+++ SuperMarketDesctopApp/Payments/Interface/IPaymentForm.cs
@@ -9,6 +9,6 @@ namespace SupermarketConsoleApp.Payments.Interface
 {
     public interface IPaymentForm
     {
-        void Count(List<Product> Basket,bool LoyaltyCard, bool CourierDelivery,CashRegister cashRegister);
+        void Count(List<Product> Basket,bool LoyaltyCard, bool CourierDelivery,CashRegister cashRegister,string onlineFormat);
     }
 }
