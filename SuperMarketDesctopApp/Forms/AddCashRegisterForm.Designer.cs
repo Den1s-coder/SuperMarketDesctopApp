defabd602d088c16fd06f6c10b854fa8d53e9cd1
Denis Katasonov <diarusfoxs@gmail.com>
2024-12-08 14:58:31 +0200

    Added: 1) Verification of the entered ID

--- SuperMarketDesctopApp/Forms/AddCashRegisterForm.Designer.cs
+++ SuperMarketDesctopApp/Forms/AddCashRegisterForm.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddCashRegisterForm";
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(142, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Вкажіть ід каси";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(133, 263);
+            button1.Name = "button1";
+            button1.Size = new Size(168, 54);
+            button1.TabIndex = 1;
+            button1.Text = "Додати касу";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(142, 167);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Модель каси";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(142, 97);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(149, 27);
+            maskedTextBox1.TabIndex = 3;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(143, 190);
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(149, 27);
+            maskedTextBox2.TabIndex = 4;
+            // 
+            // AddCashRegisterForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(416, 393);
+            Controls.Add(maskedTextBox2);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "AddCashRegisterForm";
+            Text = "AddCashRegisterForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private MaskedTextBox maskedTextBox1;
+        private MaskedTextBox maskedTextBox2;
     }
 }
--- SuperMarketDesctopApp/Forms/AddCashRegisterForm.cs
+++ SuperMarketDesctopApp/Forms/AddCashRegisterForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperMarketDesctopApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace SuperMarketDesctopApp.Forms
 {
     public partial class AddCashRegisterForm : Form
     {
-        public AddCashRegisterForm()
+        private SuperMarket _superMarket;
+
+        public event Action CashRegisterAdded;
+
+        public AddCashRegisterForm(SuperMarket superMarket)
         {
             InitializeComponent();
+            _superMarket = superMarket;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputId = maskedTextBox1.Text;
+            string inputModel = maskedTextBox2.Text;
+            if (!int.TryParse(inputId, out int id))
+            {
+                MessageBox.Show("ID повинен бути повним числом!");
+                return;
+            }
+
+            _superMarket.AddCashRegister(CashRegister.CreateCashRegister(id, inputModel,_superMarket.GetSuperMarketProducts()));
+
+            CashRegisterAdded?.Invoke();
         }
     }
 }
