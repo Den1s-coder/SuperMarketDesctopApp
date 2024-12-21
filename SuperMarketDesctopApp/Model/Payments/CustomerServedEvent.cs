1c8de34810b8c956f0685c846b8bb69bdd247ff0
Denis Katasonov <diarusfoxs@gmail.com>
2024-12-21 20:28:03 +0200

    Added Event

---
+++ SuperMarketDesctopApp/Model/Payments/CustomerServedEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketDesctopApp.Model.Payments
+{
+    public class CustomerServedEvent
+    {
+        public double TotalAmount { get; }
+        public string PaymentFormat { get; }
+        public bool IsLoyaltyCardUsed { get; }
+        public bool IsCourierDelivery { get; }
+
+        public CustomerServedEvent(double totalAmount, string paymentFormat, bool isLoyaltyCardUsed, bool isCourierDelivery)
+        {
+            TotalAmount = totalAmount;
+            PaymentFormat = paymentFormat;
+            IsLoyaltyCardUsed = isLoyaltyCardUsed;
+            IsCourierDelivery = isCourierDelivery;
+        }
+    }
+}
